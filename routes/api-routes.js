0f891af4315de384b5a83b7bebdf73af2bc6ed22
Eric Pan <pane828@gmail.com>
2017-09-23 12:56:16 -0400

    add.js & changes to new client html  & apiroutes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,31 @@
-var db = require("../models");
+var Client = require("../models/clients.js");
+var Wedding = require("../models/wedding_info.js");
 
 module.exports = function(app) {
-  app.get("/api/vendors")
-}
 
+  app.post("/api/newClient", function(req, res){
+        var client = req.body;
+
+        Client.create({
+            spouse1_first: client.spouse1_first,
+            spouse1_last: client.spouse1_last,
+            spouse2_first: client.spouse2_first,
+            spouse2_last: client.spouse2_last,
+            email: client.email,
+            phone: client.phone
+        });
+    });
+
+    app.post("/api/weddingInfo", function(req, res){
+        var info = req.body;
+
+        Wedding.create({
+            max_price: info.maxBudget,
+            date: info.date,
+            zip: info.email,
+            num_of_guests: info.numGuest
+        })
+
+    })
+
+}
