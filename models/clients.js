ea4953f8a506012a963fbc1ee654fa03d9166412
S-Voss <stephen.voss24@gmail.com>
2017-09-23 11:32:21 -0400

    Added the Foundation folder structures.

---
+++ models/clients.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  var Clients = sequelize.define("Clients", { 
+    spouse1_first: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    spouse1_last: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },   
+    spouse2_first: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    spouse2_last: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false
+    }
+  });
+};
